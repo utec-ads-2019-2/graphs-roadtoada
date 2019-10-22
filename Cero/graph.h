b1cfae642972ea49339191b3e418ff5554656f78
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2019-10-22 00:59:36 +0000

    avance

---
+++ Cero/graph.h
@@ -0,0 +1,72 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <list>
+#include <map>
+#include <iterator>
+#include "node.h"
+#include "edge.h"
+
+using namespace std;
+
+template <typename G>
+class Graph {
+public:
+    map<string,Node<G>*> nodes;
+
+    void addNode(G data){
+      if(!nodes[data->id])
+        nodes[data->id]=new Node<G>(data);
+      else
+        nodes[data->id]->data=data;
+      for(auto i:data->destinos)
+      {
+        if(!nodes[i])
+          nodes[i]=new Node<G>();
+        nodes[data->id]->edges[i]=(new Edge<G>(nodes[data->id],nodes[i]));
+      }
+    }
+
+    void addEdge(string from, string to){
+      if(nodes[from]!=0 && nodes[to]!=0)
+        nodes[from]->edges[to]=(new Edge<G>(nodes[from],nodes[to]));
+    }
+
+    Node<G>* findNode(string id){
+        return nodes[id];
+    }
+
+    Edge<G>* findEdge(string from, string to){
+        if(nodes[from]!=0 && nodes[to]!=0)
+        return nodes[from]->edges[to];
+        else return nullptr;
+    }
+
+    void removeEdge(string from,string to){
+      if(findEdge(from,to)!=0){
+        delete nodes[from]->edges[to];
+        nodes[from]->edges.erase(to);
+      }
+    }
+
+    void removeNode(string id){
+
+    }
+
+
+
+    void imprimir(){
+      for(auto i=nodes.begin();i!=nodes.end();++i){
+        cout<<"El id: "<<i->first<<endl;
+        cout<<"Aristas"<<endl;
+        nodes[i->first]->print_edges();
+        // i->second->print_edges();
+        cout<<endl;
+      }
+
+    }
+};
+
+
+#endif
---
+++ Cero/node.h
@@ -0,0 +1,56 @@
+#ifndef NODE_H
+#define NODE_H
+#include<map>
+#include<iostream>
+#include<vector>
+#include "edge.h"
+using namespace std;
+
+template<typename G>
+class Edge;
+
+
+class airport{
+public:
+  string id;
+  string city;
+  string name;
+  string country;
+  double x;
+  double y;
+  std::vector<string> destinos;
+  airport(string id_,string city_,string name_,string country_,double x,double y,vector<string> destinos_):id(id_),city(city_),name(name_),country(country_),x(x),y(y),destinos(destinos_){};
+};
+
+
+template <typename G>
+class Node {
+public:
+    G data;
+    map<string,Edge<G>*> edges;
+    Node(){};
+    Node(G data):data(data){};
+    void print_edges(){
+      for(auto i=edges.begin();i!=edges.end();++i){
+        cout<<" Origen "<<i->second->nodes[0]->data->id<<" Destino "<<i->second->nodes[1]->data->id<<endl;
+      }
+    }
+};
+
+
+template <typename G>
+class Edge {
+public:
+    typedef Node<G>* node;
+    node nodes[2];
+    Edge(node inicio,node fin){
+      nodes[0]=inicio;
+      nodes[1]=fin;
+    }
+};
+
+
+
+
+
+#endif
